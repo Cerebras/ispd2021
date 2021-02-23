a7f3dc6e4e0f963e3ad1c2fdaf156b146530bce4
Leo Wang <leo.wang@cerebras.net>
2021-02-22 16:35:51 -0800

    Add empty tests and validation

--- validator/ispd_validate.c
+++ validator/ispd_validate.c
@@ -651,28 +651,29 @@ tile_tree *build_tile_tree(tile ***tiles, struct prism *prisms, int nprims, int 
             }
         }
     }
-
-    // Sort tiles spatially for building tree
-    qsort(added_tiles, ntiles, sizeof(tile *), cmp_tiles);
-
-    // Create lowest level interior nodes
-    tile_node **interior_nodes = insert_tile(tree, added_tiles, ntiles);
-
-    // Build rest of interior nodes
-    for (; v_count(interior_nodes) > 1;)
+    if (v_count(added_tiles) > 0)
     {
-        tile_node **new_level = insert_tile_node(tree, interior_nodes, v_count(interior_nodes));
+        // Sort tiles spatially for building tree
+        qsort(added_tiles, ntiles, sizeof(tile *), cmp_tiles);
+
+        // Create lowest level interior nodes
+        tile_node **interior_nodes = insert_tile(tree, added_tiles, ntiles);
+
+        // Build rest of interior nodes
+        for (; v_count(interior_nodes) > 1;)
+        {
+            tile_node **new_level = insert_tile_node(tree, interior_nodes, v_count(interior_nodes));
+            v_free(interior_nodes);
+            interior_nodes = new_level;
+        }
+
+        tree->head = interior_nodes[0];
+
         v_free(interior_nodes);
-        interior_nodes = new_level;
+
+        // Sort tiles by id for later lookup
+        qsort(added_tiles, ntiles, sizeof(tile *), cmp_tiles_id);
     }
-
-    tree->head = interior_nodes[0];
-
-    v_free(interior_nodes);
-
-    // Sort tiles by id for later lookup
-    qsort(added_tiles, ntiles, sizeof(tile *), cmp_tiles_id);
-
     // Return added tiles in tiles pointer
     *tiles = added_tiles;
     return tree;
@@ -733,7 +734,6 @@ void build_connectivity()
     all_connections = calloc(1, sizeof(struct connectivity));
 
     int ntiles = v_count(tile_list);
-
     for (int i = 0; i < ntiles; i += 1)
     {
         tile *t = tile_list[i];
@@ -1895,7 +1895,55 @@ void validate_empty_prisms()
 {
     for (int i = 0; i < sol->nprims; i += 1)
     {
-        struct prism *p = sol->prism[i];
+        struct prism *prism = &sol->prism[i];
+        if (!prism->is_empty)
+        {
+            continue;
+        }
+
+        tile *t = NULL;
+
+        if (dimension == 2)
+        {
+            for (int x = 0; x < prism->shape[0]; x += 1)
+            {
+                for (int y = 0; y < prism->shape[1]; y += 1)
+                {
+                    int coords[2] = {x, y};
+                    t = get_tile(prism, i, 0, coords);
+                    if (t != NULL)
+                    {
+                        node_bounds_d scaled = scale_tile_to_heatmap(t);
+                        double i2 = integrate_y(scaled.low_bounds[0], scaled.up_bounds[0], scaled.low_bounds[1], scaled.up_bounds[1], 0);
+
+                        fatalif(abs_d(i2) > DOUBLE_EPSILON, "Prism %d cannot be empty. It encloses a non-empty target resolution %f", i, i2);
+                        free(t);
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int x = 0; x < prism->shape[0]; x += 1)
+            {
+                for (int y = 0; y < prism->shape[1]; y += 1)
+                {
+                    for (int z = 0; z < prism->shape[2]; z += 1)
+                    {
+                        int coords[3] = {x, y, z};
+                        t = get_tile(prism, i, 0, coords);
+                        if (t != NULL)
+                        {
+                            node_bounds_d scaled = scale_tile_to_heatmap(t);
+                            double i2 = integrate_z(scaled.low_bounds[0], scaled.up_bounds[0], scaled.low_bounds[1], scaled.up_bounds[1], scaled.low_bounds[2], scaled.up_bounds[2]);
+
+                            fatalif(abs_d(i2) > DOUBLE_EPSILON, "Prism %d cannot be empty. It encloses a non-empty target resolution %f", i, i2);
+                            free(t);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -1915,6 +1963,8 @@ int validate()
         printf("[OK] - Connectivity Validated\n");
         validate_pe_fit();
         printf("[OK] - PE Fit Validated\n");
+        validate_empty_prisms();
+        printf("[OK] - Empty Prisms Validated\n");
     }
 
     return adapters_good;
